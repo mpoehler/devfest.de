36a20c2389d668fbd6b11ea37d6bb3bdfed7a4f0
Marco Poehler <marco@kontaktlinsen-preisvergleich.de>
2016-08-31 11:08:01 +0200

    added letsencrypt challenge for devfest.de and www.devfest.de

--- letsencrypt.py
+++ letsencrypt.py
@@ -4,8 +4,8 @@ class LetsEncryptHandler(webapp2.RequestHandler):
     def get(self, challenge):
         self.response.headers['Content-Type'] = 'text/plain'
         responses = {
-            'beguyUUE_XH8uCPHxKlB2Ts89Xdoc_Cp53EHf1iz7s4': 'beguyUUE_XH8uCPHxKlB2Ts89Xdoc_Cp53EHf1iz7s4.ONja4hoK8i3BGvn-QMf9BKbPyaNRQ3FkecClr-1On6g',
-            '[challenge 2]': '[response 2]'
+            'rQdM6WAmon1pH-YRI-mbyfPIND6hqcNwCH8q_nzxqwI': 'rQdM6WAmon1pH-YRI-mbyfPIND6hqcNwCH8q_nzxqwI.DlYoCObwrBzgQU4uXQdkPNEsRrNNNHlHxzzD_ZYYRZk',
+            'BJK3wReUL97dkNYOlHN2L-9yzZv3czXj7Ck2oQ_1z84': 'BJK3wReUL97dkNYOlHN2L-9yzZv3czXj7Ck2oQ_1z84.DlYoCObwrBzgQU4uXQdkPNEsRrNNNHlHxzzD_ZYYRZk'
         }
         self.response.write(responses.get(challenge, ''))
 
